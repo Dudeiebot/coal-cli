0c00d8a7ff6b5efab598f98df0c8668f7363bec5
Dudeiebot <Ojoexcel5@gmail.com>
2024-08-12 21:02:45 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -35,6 +35,7 @@ struct Miner {
     pub dynamic_fee: bool,
     pub rpc_client: Arc<RpcClient>,
     pub fee_payer_filepath: Option<String>,
+    pub jito_client: Arc<RpcClient>,
 }
 
 #[derive(Subcommand, Debug)]
@@ -133,6 +134,14 @@ struct Args {
     #[arg(long, help = "Enable dynamic priority fees", global = true)]
     dynamic_fee: bool,
 
+     #[arg(
+        long,
+        value_name = "JITO", 
+        help = "Add jito tip to the miner. Defaults to false.",
+        global = true
+    )]
+    jito: bool,
+
     #[command(subcommand)]
     command: Commands,
 }
@@ -158,6 +167,31 @@ async fn main() {
     let default_keypair = args.keypair.unwrap_or(cli_config.keypair_path.clone());
     let fee_payer_filepath = args.fee_payer.unwrap_or(default_keypair.clone());
     let rpc_client = RpcClient::new_with_commitment(cluster, CommitmentConfig::confirmed());
+    let jito_client =
+        RpcClient::new("https://mainnet.block-engine.jito.wtf/api/v1/transactions".to_string());
+
+    let tip = Arc::new(RwLock::new(0_u64));
+    let tip_clone = Arc::clone(&tip);
+
+    if args.jito {
+        let url = "ws://bundles-api-rest.jito.wtf/api/v1/bundles/tip_stream";
+        let (ws_stream, _) = connect_async(url).await.unwrap();
+        let (_, mut read) = ws_stream.split();
+
+        tokio::spawn(async move {
+            while let Some(message) = read.next().await {
+                if let Ok(Message::Text(text)) = message {
+                    if let Ok(tips) = serde_json::from_str::<Vec<Tip>>(&text) {
+                        for item in tips {
+                            let mut tip = tip_clone.write().unwrap();
+                            *tip =
+                                (item.landed_tips_50th_percentile * (10_f64).powf(9.0)) as u64;
+                        }
+                    }
+                }
+            }
+        });
+    }
 
     let miner = Arc::new(Miner::new(
         Arc::new(rpc_client),
@@ -166,6 +200,8 @@ async fn main() {
         args.dynamic_fee_url,
         args.dynamic_fee,
         Some(fee_payer_filepath),
+        Arc::new(jito_client),
+        tip,
     ));
 
     // Execute user command.
@@ -226,6 +262,8 @@ impl Miner {
             dynamic_fee_url,
             dynamic_fee,
             fee_payer_filepath,
+            jito_client,
+            tip,
         }
     }
 
